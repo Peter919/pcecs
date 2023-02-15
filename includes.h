19bf6e9165e9458a25f0901b71ad9163c03fa5c5
Peter919 <97379969+Peter919@users.noreply.github.com>
2023-02-15 07:33:59 +0100

    Add files via upload

---
+++ includes.h
@@ -0,0 +1,16 @@
+// Everything from pcecs that should be included externally.
+
+#ifndef PCECS_INCLUDES_H
+#define PCECS_INCLUDES_H
+
+#include "interface/init.h"
+
+#include "interface/entity.h"
+#include "interface/ct.h"
+#include "interface/sys.h"
+#include "interface/sys_funcs.h"
+
+#include "interface/ct_set.h"
+#include "interface/cgroup.h"
+
+#endif
